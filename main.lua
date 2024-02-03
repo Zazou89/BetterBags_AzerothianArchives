3a27e1d4dc6f51d0ce45bcb12d92cb941b12faf2
Zazou89 <155338110+Zazou89@users.noreply.github.com>
2024-02-03 22:12:45 +0100

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,69 @@
+---@class BetterBags: AceAddon
+local addon = LibStub('AceAddon-3.0'):GetAddon("BetterBags")
+
+---@class Categories: AceModule
+local categories = addon:GetModule('Categories')
+
+---@class Localization: AceModule
+local L = addon:GetModule('Localization')
+
+--Mysterious Fragments Containers
+local Containers = {
+    200285, --Dragonscale Expedition Insignia (Green)
+    213389, --Ancient Centaur Diary
+    213025, --Ancient Djaradin Bolt
+    213020, --Ancient Drakonid Shovel
+    213024, --Bloody Djaradin Mug
+    213200, --Book of Horoscopes
+    213365, --Carved Cave Crystal
+    213215, --Carved Votive Stone
+    213021, --Corked Drakonid Vial
+    213359, --Cracked Drakonid Staff
+    212687, --Crude Toy Ducky
+    213357, --Crystal Scrying Bowl
+    212978, --Dragon Painter's Palette
+    213022, --Drakonid Blacksmith Hammer
+    212976, --Drakonid Duck Carving
+    212977, --Drakonid Sign Placard
+    213183, --Makeshift Beaded Bracelet
+    213023, --Oil-Drenched Djaradin Torch
+    205223, --Ornamented Centaur Axe
+    213382, --Partial Centaur Hunting Map
+    212782, --Poorly Preserved Relic
+    213208, --Pot of Pickled Ancheevies
+    212650, --Pot of Tattooing Ink
+    213204, --Ruby-Studded Locket
+    212773, --Rusted Locket
+    213375, --Satchel of Buried Valuables
+    213192, --Scratched-Out List of Names
+    212762, --Tarnished Horn Signet
+    212769, --Tattered Wanted Poster
+}
+--Big Dig Tomes
+local BigDigTomes = {
+    213185, --Dusty Centaur Tome
+    213175, --Dusty Djaradin Tome
+    213188, --Dusty Dracthyr Tome
+    213187, --Dusty Drakonid Tome
+    213186, --Dusty Niffen Tome
+    213190, --Preserved Djaradin Tome
+    213189, --Preserved Dragonkin Tome
+    213176, --Preserved Isles Tome
+    213177, --Immaculate Tome
+}
+--Miscellaneous
+local Miscellaneous = {
+    213536, --Priceless Artifact
+}
+
+local allItems = {
+    Containers,
+    Miscellaneous,
+    BigDigTomes
+}
+
+for _, itemList in pairs(allItems) do
+    for _, ItemID in pairs(itemList) do
+        categories:AddItemToCategory(ItemID, "Azerothian Archives")
+    end
+end
